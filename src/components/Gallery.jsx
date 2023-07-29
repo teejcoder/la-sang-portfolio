7e4fb0f9a7a35888d68f0bad70f93d68519c46eb
Teej <96551689+teejcoder@users.noreply.github.com>
2023-07-29 19:40:08 +1000

    latest changes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,25 +8,25 @@ export default function Gallery() {
       <section className='gallery'>
 
         <Link to="/shag-rock">
-          <div className='gallery-div'>
+          <div className='gallery-div shag-rock-div'>
               <img src="/assets/shag-rock/shag-rock-cover.png" alt="" className='gallery-img shag-rock'/>
           </div>
         </Link>
 
         <Link to="/confidence-man">
-          <div className='gallery-div'>
+          <div className='gallery-div confidence-man-div'>
               <img src="/assets/confidence-man/confidence-man-cover.png" alt="" className='gallery-img confidence-man'/>
           </div>
         </Link>
         
         <Link to="/hallie">
-          <div className='gallery-div'>
+          <div className='gallery-div hallie-div'>
               <img src="/assets/hallie/hallie-cover.png" alt="" className='gallery-img hallie'/>
           </div>
         </Link>
         
         <Link to="/lime-cordial">
-          <div className='gallery-div'>
+          <div className='gallery-div lime-cordial-div'>
               <img src="/assets/lime-cordial/lime-cordial-cover.png" alt="" className='gallery-img lime-cordial'/>
           </div>
         </Link>
